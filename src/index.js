2b9d191a582037ed234f5e79c333cfe7fc5c5650
hadar lavsky <hadarlavsky@gmail.com>
2025-05-03 01:19:29 +0300

    fix devmode

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const options = {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {process.env.REACT_APP_DEV==="true" ? (
+    {process.env.REACT_APP_DEV==="false" ? (
       <PostHogProvider apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY} options={options}>
         <AppRoutes />
       </PostHogProvider>
